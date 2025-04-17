e8dfc4904791a87809404342d40c7193fcc62f28
Sana Elshamy <snelshamy@gmail.com>
2025-04-17 13:15:34 +0200

    Initial Commit

---
+++ WaveForm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaveForm
+//
+//  Created by Sana on 17/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaveForm/WaveFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveFormApp.swift
+//  WaveForm
+//
+//  Created by Sana on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WaveFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
